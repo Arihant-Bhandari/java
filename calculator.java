1bf9cc3971210418b83e5ea5267343c40fa5a6a2
Arihant Bhandari <127655293+Arihant-Bhandari@users.noreply.github.com>
2023-08-18 23:51:28 +0530

    updated code

--- calculator.java
+++ calculator.java
@@ -3,48 +3,60 @@ import java.util.Scanner;
 import java.lang.*;
 public class calculator
 {
-    static Scanner sc = new Scanner(System.in);
-    static double result = 0;
-    public static int first()
+    Scanner sc = new Scanner(System.in);
+    double result = 0;
+        public static void main(String arg[]) throws IOException
+    {
+        System.out.println("Welcome ! this is a demo calculator.");
+        
+        calculator cal = new calculator();
+        
+        cal.result = cal.first();
+        System.out.println("enter from within these functions: *,/,-,+,%,^,root");
+        String s = cal.sc.next();
+        System.out.println(cal.arith_calc(s));
+        }
+    
+    public float first()
     {
         System.out.println("enter first value:");
-        int f = sc.nextInt();
+        float f = sc.nextFloat();
         return f;
     }
-    public static double arith_calc(String s)
+    public double arith_calc(String s)
     {
         if(s.equals("="))
         {
             return result;   
         }
-        int n = 0;
+        float n = 0;
         if(s.equals("/") || s.equals("*") || s.equals("-") || s.equals("+") || s.equals("%"))
         {
             System.out.println("enter value:");
         
-            n = sc.nextInt();
+            n = sc.nextFloat();
         }
         switch(s)
             {
                 case "+":
                     result = result + n;
-                    System.out.println("enter from within these functions: *,/,-,+,%,^,root");
+                    System.out.println("Enter from within these functions: *,/,-,+,%,^,root.\nuse = to display final answer.");
                     return arith_calc(sc.next());
                 case "-":
                     result = result - n;
-                    System.out.println("enter from within these functions: *,/,-,+,%,^,root");
+                    System.out.println("Enter from within these functions: *,/,-,+,%,^,root.\nuse = to display final answer.");
                     return arith_calc(sc.next());
                 case "*":
                     result = result * n;
-                    System.out.println("enter from within these functions: *,/,-,+,%,^,root");
+                    System.out.println("Enter from within these functions: *,/,-,+,%,^,root.\nuse = to display final answer.");
                     return arith_calc(sc.next());
                 case "/":
                     result = result / n;
-                    System.out.println("enter from within these functions: *,/,-,+,%,^,root");
+                    System.out.println("Enter from within these functions: *,/,-,+,%,^,root.\nuse = to display final answer.");
                     return arith_calc(sc.next());
                 case "%":
                     result = result % n;
-                    System.out.println("enter from within these functions: *,/,-,+,%,^,root");
+                    System.out.println("Enter from within these functions: *,/,-,+,%,^,root.\nuse = to display final answer.");
                     return arith_calc(sc.next());
                 case "^":
                     System.out.println("enter exponent.");
@@ -58,14 +70,4 @@ public class calculator
                     return 0;
             }
     }
-    public static void main(String arg[]) throws IOException
-    {
-        System.out.println("Welcome ! this is a demo calculator.");
-        
-        result = first();
-        System.out.println("enter from within these functions: *,/,-,+,%,^,root");
-        String s = sc.next();
-        System.out.println(arith_calc(s));
-        }
-    }
-
+}
