f1efdf99647df96431b723dec60e3d95214704e8
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2024-04-22 00:03:44 +0530

    Matrix Addition

---
+++ MatrixAdd.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class MatrixAdd
+{
+    public static void main(String arg[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Dimensions of Matrices: ");
+        int n = sc.nextInt();
+
+        int a[][] = new int[n][n];
+        int b[][] = new int[n][n];
+
+        System.out.println();
+
+        System.out.println("Matrix 1: ");
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                System.out.print("Enter element: ");
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Matrix 2: ");
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                System.out.print("Enter element: ");
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println();
+        
+        MatrixAdd add = new MatrixAdd();
+
+        add.matAdd(a,b);
+
+        sc.close();
+    }
+
+    public void matAdd(int a[][],int b[][])
+    {
+        int c[][] = new int[a.length][a.length];
+
+        for(int i = 0; i < a.length; i++)
+        {
+            for(int j = 0; j < a.length; j++)
+            {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        System.out.println("Resultant Matrix: ");
+        for(int i = 0; i < a.length; i++)
+        {
+            for(int j = 0; j < a.length; j++)
+            {
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
